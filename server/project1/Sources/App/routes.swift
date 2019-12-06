54af9d1b078f948708503126a55fa1c8b9877608
Joshua Buhler <josh@buhler.me>
2019-12-05 17:46:55 -0700

    Beginning futures

--- server/project1/Sources/App/routes.swift
+++ server/project1/Sources/App/routes.swift
@@ -14,10 +14,8 @@ public func routes(_ router: Router) throws {
         return try req.view().render("home", context)
     }
     
-    router.get("staff") { req in
-        return "Meet our great team"
-    }
-    router.get("contact") { req in
-        return "Get in touch with us"
+    router.get("contact") { req -> Future<View> in
+        let context = [String: String]()
+        return try req.view().render("contact", context)        
     }
 }
