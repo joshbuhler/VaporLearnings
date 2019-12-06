f94a1bbf5182a1b616a6cfbf1f685e1cdbd387f1
Joshua Buhler <josh@buhler.me>
2019-12-05 18:36:07 -0700

    Adding staff template and working dynamic routes

--- server/project1/Sources/App/routes.swift
+++ server/project1/Sources/App/routes.swift
@@ -18,4 +18,28 @@ public func routes(_ router: Router) throws {
         let context = [String: String]()
         return try req.view().render("contact", context)        
     }
+    
+    router.get("staff", String.parameter) { req -> Future<View> in let name = try req.parameters.next(String.self)
+        // create some dummy data to work with
+        let bios = [
+            "kirk": "My name is James Kirk and I love snakes.", "picard": "My name is Jean-Luc Picard and I'm mad for fish.",
+            "sisko": "My name is Benjamin Sisko and I'm all about the            budgies.",
+            "janeway": "My name is Kathryn Janeway and I want to hug            every hamster.",
+            "archer": "My name is Jonathan Archer and beagles are my            thing."
+        ]
+        // define the struct we'll pass to the template
+        struct StaffView: Codable {
+            var name: String?
+            var bio: String?
+        }
+        
+        let context: StaffView
+        // attempt to find a staff member by this name and fill in our struct
+        if let bio = bios[name] {
+            context = StaffView(name: name, bio: bio)
+        } else {
+            context = StaffView() }
+        // render the template with whatever we have
+        return try req.view().render("staff", context)
+    }
 }
