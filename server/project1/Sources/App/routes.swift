a15f389e021fa29441189858f8b09d64980a1832
Joshua Buhler <josh@buhler.me>
2019-12-05 17:41:03 -0700

    Project import

---
+++ server/project1/Sources/App/routes.swift
@@ -0,0 +1,23 @@
+import Routing
+import Vapor
+import Foundation
+import Leaf
+
+
+/// Register your application's routes here.
+///
+/// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
+public func routes(_ router: Router) throws {
+    
+    router.get { req -> Future<View> in
+        let context = [String: String]()
+        return try req.view().render("home", context)
+    }
+    
+    router.get("staff") { req in
+        return "Meet our great team"
+    }
+    router.get("contact") { req in
+        return "Get in touch with us"
+    }
+}
