360f8d2a8391803bd00b3cd4a848f3d977ee568c
Joshua Buhler <josh@buhler.me>
2019-12-19 19:54:29 -0700

    grouping

--- server/project3/Sources/App/routes.swift
+++ server/project3/Sources/App/routes.swift
@@ -5,10 +5,10 @@ import Vapor
 ///
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
 public func routes(_ router: Router) throws {
-    router.get("hello", String.parameter) { req -> String in
-        let name = try req.parameters.next(String.self)
-        return "Hello, \(name)"
-    }
+//    router.get("hello", String.parameter) { req -> String in
+//        let name = try req.parameters.next(String.self)
+//        return "Hello, \(name)"
+//    }
     
     router.get("articles", Article.parameter) { req -> String in
         let article = try req.parameters.next(Article.self)
@@ -17,5 +17,15 @@ public func routes(_ router: Router) throws {
     
     router.get("articles", Article.parameter, "json") { req -> Article in
         return try req.parameters.next(Article.self)
+    }
+    
+    router.group("hello") { group in
+        group.get("world") { req in
+            return "Hello, world"
+        }
+        
+        group.get("kitty") { req in
+            return "Hello, kitty"
+        }
     }
 }
