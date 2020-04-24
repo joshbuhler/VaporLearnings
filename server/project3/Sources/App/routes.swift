dd5f7a73d5f7fa99b16d1f1025d3cae0c4dfa766
Joshua Buhler <josh@buhler.me>
2020-04-24 17:38:48 -0600

    Building up routes

--- server/project3/Sources/App/routes.swift
+++ server/project3/Sources/App/routes.swift
@@ -8,4 +8,29 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
+    router.get("hello", "world") { req in
+        return "Hello, world! 2"
+    }
+    
+    router.get("greetings", String.parameter) { req -> String in
+        let name = try req.parameters.next(String.self)
+        return "Hello, \(name)"
+    }
+    
+    router.get("read", Int.parameter) { req -> String in
+        let number = try req.parameters.next(Int.self)
+        return "Now it's time for page \(number)"
+    }
+    
+    router.get("posts", Int.parameter, String.parameter) { req -> String in
+        let id = try req.parameters.next(Int.self)
+        let title = try req.parameters.next(String.self)
+        return "Loading article: \(id) - \(title)"
+    }
+    
+    router.post("posts", Int.parameter, String.parameter) { req -> String in
+        let id = try req.parameters.next(Int.self)
+        let title = try req.parameters.next(String.self)
+        return "Saving article: \(id) - \(title)"
+    }
 }
